9d5ea3f3551c1a3e34e23651f3e045bb5c413eee
KSathish07 <satishk@pakricorn.com>
2026-05-19 14:39:51 +0530

    second change

--- customer-management/src/main/java/com/example/customer/controller/CustomerController.java
+++ customer-management/src/main/java/com/example/customer/controller/CustomerController.java
@@ -16,7 +16,7 @@ import com.example.customer.service.CustomerService;
 public class CustomerController {
 	@GetMapping("/welcome")
 	public String welcome() {
-		return "welcome to new customer";
+		return "welcome to customer";
 	}
 	
 	@Autowired
